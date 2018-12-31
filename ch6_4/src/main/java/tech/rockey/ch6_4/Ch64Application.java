442c38a82d157486a237ed5f162894592229c8fd
rockey <1228863840@qq.com>
2018-12-31 16:09:58 +0800

    Springboot 生产环境，测试环境文件配置

---
+++ ch6_4/src/main/java/tech/rockey/ch6_4/Ch64Application.java
@@ -0,0 +1,14 @@
+package tech.rockey.ch6_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch64Application.class, args);
+	}
+
+}
+
