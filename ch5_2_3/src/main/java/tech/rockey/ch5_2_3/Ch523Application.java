1814f38edbd3492080c1c33697064851f0c8e4d6
rockey <1228863840@qq.com>
2018-12-31 14:54:31 +0800

    测试WEB功能，文件配置修改

--- ch5_2_3/src/main/java/tech/rockey/ch5_2_3/Ch523Application.java
+++ ch5_2_3/src/main/java/tech/rockey/ch5_2_3/Ch523Application.java
@@ -1,10 +1,26 @@
 package tech.rockey.ch5_2_3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@ConfigurationProperties(prefix = "book")
+@RestController
 @SpringBootApplication
 public class Ch523Application {
+	@Value("${book.author}")
+	private String bookAuthor;
+
+	@Value("${book.name}")
+	private String bookName;
+
+	@RequestMapping("/")
+		String index(){
+		return "bookname is:" + bookName + " and bookAuthor is: " + bookAuthor;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ch523Application.class, args);
